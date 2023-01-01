c388645d23722710de4b15a19e5eaebb9d135c4e
Mahesh974I <maheshmudduluru455@gmail.com>
2023-01-01 11:33:40 +0530

    1

--- test_login.py
+++ test_login.py
@@ -6,7 +6,10 @@ class Testlogin:
     def test_01_my_prezentation(self):
         # n = random.randint(1, 8)
         self.driver.get('https://uatstaging.myprezent.com/signin')
-        self.driver.find_element('id','username').send_keys('amod-uat.noreply@abbvie.com ')  ## sending user name to browser
+        time.sleep(5)
+        self.driver.find_element('id','username').send_keys('amod-uat.noreply@abbvie.com ')
+        ## sending user name to browser
+        time.sleep(5)
         self.driver.find_element('id','password').send_keys('7b6907195ed41d261bd9')
         self.driver.find_element('id','submit').click()
         time.sleep(20)
