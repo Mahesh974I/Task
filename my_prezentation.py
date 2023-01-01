5823133eaf2eb2708c3b15ca7863621612ff6203
Mahesh974I <maheshmudduluru455@gmail.com>
2023-01-01 11:49:26 +0530

    1

--- my_prezentation.py
+++ my_prezentation.py
@@ -2,17 +2,24 @@ from selenium import webdriver
 import time
 import random
 
-class login():
+from selenium.webdriver.common.by import By
+
+
+class Prezentation():
 
     def test_01_my_prezentation(self):
-        # n = random.randint(1, 8)
-        driver = webdriver.Firefox()
-        driver.get('https://uatstaging.myprezent.com/signin')
-        driver.find_element('id','username').send_keys('directorvbxq5.noreply@prezent.ai')  ## sending user name to browser
-        driver.find_element('id','password').send_keys('prezent123')
-        driver.find_element('id','submit').click()
+        self.driver.get('https://livestaging.myprezent.com/signin')
+        time.sleep(5)
+        self.driver.find_element(By.CSS_SELECTOR, '#username').send_keys('ayush-ls.noreply@abbvie.com')
+        self.driver.find_element(By.CSS_SELECTOR, '#continue').click()
+
+        ## sending user name to browser
+        # time.sleep(5)
+        self.driver.find_element(By.CSS_SELECTOR, '#password').send_keys('870a099a729a3de2ca1d')
+        self.driver.find_element(By.CSS_SELECTOR, '#submit').click()
         time.sleep(20)
-        print('Login Successfull')
+        self.driver.find_element(By.CSS_SELECTOR, 'a#v-step-2').click()
+        time.sleep(20)
     # def download():
     #     driver.find_element('xpath',"//a[@id='v-step-1']").click()
     #     time.sleep(20)
